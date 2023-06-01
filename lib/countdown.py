44dae948b0e4e5e332b0a44bfcbe3849190bab26
Mark Nosal <mark.c.nosal@gmail.com>
2023-05-31 19:16:11 -0500

    finished lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+
+def countdown(int):
+    while int > 0:
+        print(f"{int} SECOND(S)!")
+        int -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(int):
+    while int > 0:
+        print(f"{int} SECOND(S)!")
+        int -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
